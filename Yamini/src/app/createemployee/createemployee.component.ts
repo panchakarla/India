0af6f14964a9704a94bd01d8e380bcb35034bf7f
Yamini <yamini0268@gmail.com>
2018-11-30 11:32:25 -0600

    addded department drop down to the form

--- Yamini/src/app/createemployee/createemployee.component.ts
+++ Yamini/src/app/createemployee/createemployee.component.ts
@@ -1,3 +1,4 @@
+import { Department } from './../Department.model';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -7,6 +8,12 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./createemployee.component.css']
 })
 export class CreateemployeeComponent implements OnInit {
+
+  departments: Department[] = [
+    {id: 1, name: 'IT HELP Desk'},
+    {id: 2, name: 'Team Lead'},
+    {id: 3, name: 'Developer'}
+  ];
 
   constructor() { }
   ngOnInit() {
